a954623f25e89ec661e775aa9c0471fcdfe89cf6
vaibhavgupta082 <86800548+vaibhavgupta082@users.noreply.github.com>
2026-07-26 13:38:25 +0530

    Configure WhiteNoise for static file handling
    Added WhiteNoise middleware and configured static file settings for production.

--- typing_master/settings.py
+++ typing_master/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,6 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# Directory where collectstatic will gather static files for production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Tell Django to use WhiteNoise to compress and serve static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
